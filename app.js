c0e4a101b2ed0036d3274d310662581728f6ccd7
Jung H Davis <cy4jcd@yahoo.com>
2018-10-24 18:17:23 -0700

    classlab03 commit 0617pm

---
+++ app.js
@@ -0,0 +1,69 @@
+"use strict";
+alert('Please respond to the following questions with Y/ y/ YES/ Yes/ yes OR N/ n/ NO/ No,');
+
+console.log(responseOne);
+
+var responseOne = prompt('Was I born in WA state?').toUpperCase();
+if (responseOne === 'Y' || responseOne === 'YES') {
+  alert('Sorry, That is incorrect.');
+} else {
+  alert('You are correct. I was not born in WA state.');
+}
+console.log(responseOne);
+
+console.log(responseTwo);
+var responseTwo = prompt('Do I like to drive?').toUpperCase();
+if (responseTwo === 'Y'|| responseTwo === 'YES') {
+  alert('Yes I like to drive.');
+}
+else {
+  alert ('No, I actually like to drive.');
+}
+console.log(responseTwo);
+
+console.log(responseThree);
+var responseThree = prompt ('Am I Right Handed?') .toUpperCase();
+if (responseThree === 'N'|| responseThree === 'NO'){
+  alert('Actually I am right handed.');
+}
+else if (responseThree === 'YES' || responseThree === 'Y') {
+  alert ('YES, I am right handed.');
+}
+console.log(responseThree);
+
+console.log(responseFour);
+var responseFour = prompt ('Am i Short?').toUpperCase();
+if  (responseFour === 'Y' || responseFour === 'YES'){
+  alert ('Well if you say so.');
+} 
+else {
+  alert ('I like to think i am tall.');
+}
+console.log(responseFour);
+
+console.log(responseFive);
+var responseFive = prompt ('Am i tired?').toUpperCase();
+if (responseFive === 'Y' || responseFive === 'YES'){
+  alert ('Yeah i used to work night shift. So i am adjusting to the day time routine.');
+}
+else if (responseFive === 'N' || responseFive === 'NO'){
+  alert ('Actually i am very tired from switching shift around.');
+}
+console.log(responseFive);
+
+console.log(numberGuess);
+
+var numberGuess = parseInt(prompt ('Between 1 to 50. What number am i guessing?'));
+var counter = 3;
+while (numberGuess !== 23 && counter > 0) {
+  counter--; 
+
+  if (numberGuess < 23) {
+    numberGuess = parseInt(prompt('too low guess again.'));
+  } else {
+    numberGuess = parseInt(prompt('too high. guess again.'));
+  } if (numberGuess === 23) {
+    alert ('Yay. You got it.');
+  }
+}
+console.log(numberGuess);
